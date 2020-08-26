7f266e89bbefb23bd8130547327d685818b402aa
Manuel Bertrams <mail@bertrams-media.de>
2020-08-26 12:09:58 +0200

    init var in ExtensionChain

--- Extension/ExtensionChain.php
+++ Extension/ExtensionChain.php
@@ -5,15 +5,14 @@ namespace TheCocktail\Bundle\SuluSchemaOrgBundle\Extension;
 use Spatie\SchemaOrg\BaseType;
 use TheCocktail\Bundle\SuluSchemaOrgBundle\Extension\ExtensionInterface;
 
-class ExtensionChain
-{
+class ExtensionChain {
+
     /**
      * @var ExtensionInterface[]
      */
-    private array $extensions;
+    private array $extensions = [];
 
-    public function addExtension(ExtensionInterface $extension): void
-    {
+    public function addExtension(ExtensionInterface $extension): void {
         $this->extensions[] = $extension;
     }
 
@@ -22,13 +21,12 @@ class ExtensionChain
      * @param BaseType[] $defined
      * @return mixed
      */
-    public function extend(BaseType $baseType, array $defined)
-    {
+    public function extend(BaseType $baseType, array $defined) {
         foreach ($this->extensions as $extension) {
             if (in_array(get_class($baseType), $extension->getTypes())) {
                 $extension->extend($baseType, $defined);
             }
         }
-
     }
+
 }
